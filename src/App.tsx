0d8b0c20b538445185d0026b238b18cecc78d1fd
mikefrawth <mfroth691@gmail.com>
2026-07-13 16:16:02 -0400

    made a counter that increments and decrements

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = (i: number = 1) => {
+    setCount(count + i);
+  };
+
+  const handleDecrement = (i: number = 1) => {
+    setCount(count - i);
+  };
+
+  return (
+    <div>
+      <h1>TypeScript Playground</h1>
+      <p>Count: {count}</p>
+      <button
+        onClick={() => {
+          const inputElement = document.querySelector<HTMLInputElement>(
+            'input[name="counter"]'
+          );
+          const value = inputElement
+            ? parseInt(inputElement.value, 10) || 1
+            : 1;
+          handleIncrement(value);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          const inputElement = document.querySelector<HTMLInputElement>(
+            'input[name="counter"]'
+          );
+          const value = inputElement
+            ? parseInt(inputElement.value, 10) || 1
+            : 1;
+          handleDecrement(value);
+        }}
+      >
+        Increment
+      </button>
+      <div>
+        <label htmlFor="counter"></label>
+        <input type="number" name="counter"></input>
+      </div>
+    </div>
+  );
+}
+
+export default App;
